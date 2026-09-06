b662ebd15226747a5110bb2947f4a66a385c2d6e
TycoonYT <tycoonytzyx1234@gmail.com>
2026-09-07 00:56:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoApp from './Components/TodoApp/TodoApp'
+
+const App = () => {
+  return (
+    <div>
+      <TodoApp />
+    </div>
+  )
+}
+
+export default App
